aad406dc368026408d8da148d62708577629b650
CarolinaCapilla <carolina.capilla@gmail.com>
2020-07-21 20:54:11 +0100

    created seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# require 'json'
+# require 'rest-client'
+
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+# cocktail_ingredients = JSON.parse(RestClient.get(url))
+
+# 20.times do
+#   Ingredient.create()
+# end
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating ingredients'
+ingredients = [
+  Ingredient.create(name: 'lemon'),
+  Ingredient.create(name: 'lime'),
+  Ingredient.create(name: 'mint leaves'),
+  Ingredient.create(name: 'white rum'),
+  Ingredient.create(name: 'gomme'),
+  Ingredient.create(name: 'gin'),
+  Ingredient.create(name: 'campari'),
+  Ingredient.create(name: 'orange juice'),
+  Ingredient.create(name: 'tequila blanco'),
+  Ingredient.create(name: 'orgeat'),
+  Ingredient.create(name: 'triple sec'),
+  Ingredient.create(name: 'rose essence')
+]
+
+ingredients.each do |ingredient|
+  puts "created #{ingredient.name}"
+end
+
+puts 'Finished'
