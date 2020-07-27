83b493c2668a2becca3b642c9cf6637dc4df5888
CarolinaCapilla <carolina.capilla@gmail.com>
2020-07-27 15:09:01 +0100

    added images upload functionality + styling to index/new/show

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,20 +13,18 @@ Ingredient.destroy_all
 
 puts 'Creating ingredients'
 ingredients = [
-  Ingredient.create(name: 'lemon'),
-  Ingredient.create(name: 'lime'),
-  Ingredient.create(name: 'mint leaves'),
-  Ingredient.create(name: 'white rum'),
-  Ingredient.create(name: 'gomme'),
-  Ingredient.create(name: 'gin'),
-  Ingredient.create(name: 'campari'),
-  Ingredient.create(name: 'orange juice'),
-  Ingredient.create(name: 'tequila blanco'),
-  Ingredient.create(name: 'orgeat'),
-  Ingredient.create(name: 'triple sec'),
-  Ingredient.create(name: 'rose essence'),
-  Ingredient.create(name: 'campari'),
-  Ingredient.create(name: 'orange juice')
+  Ingredient.create(name: 'Lemon'),
+  Ingredient.create(name: 'Lime'),
+  Ingredient.create(name: 'Mint leaves'),
+  Ingredient.create(name: 'White rum'),
+  Ingredient.create(name: 'Gomme'),
+  Ingredient.create(name: 'Gin'),
+  Ingredient.create(name: 'Campari'),
+  Ingredient.create(name: 'Orange juice'),
+  Ingredient.create(name: 'Tequila blanco'),
+  Ingredient.create(name: 'Orgeat'),
+  Ingredient.create(name: 'Triple sec'),
+  Ingredient.create(name: 'Rose essence'),
 ]
 
 ingredients.each do |ingredient|
